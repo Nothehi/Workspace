ddc1a1e9ab511db564212c4cb4bd3208c73f3da0
nothehi <nodehi7813@gmail.com>
2025-07-15 11:39:45 +0330

    fix: php now can make fake image

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,15 +1,37 @@
-FROM php:8.2-cli
+FROM php:8.4-cli
 
 # Install system dependencies and PHP extensions
 RUN apt-get update && apt-get install -y \
     git \
+    curl \
+    nano \
+    zip \
     unzip \
-    libzip-dev \
-    libonig-dev \
-    libxml2-dev \
+    libgd-dev \
+    libicu-dev \
     libpq-dev \
-    && docker-php-ext-install zip sockets pcntl pdo pdo_pgsql \
-    && rm -rf /var/lib/apt/lists/*
+    libpng-dev \
+    libjpeg-dev \
+    libonig-dev \
+    libzip-dev \
+    libxml2-dev \
+    libmcrypt-dev \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libwebp-dev \
+    libxpm-dev
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
+RUN docker-php-ext-install zip soap mbstring exif bcmath sockets pcntl pdo pdo_pgsql intl \
+    && docker-php-ext-configure intl \
+    && docker-php-ext-enable intl
+
+RUN docker-php-ext-configure gd --with-jpeg \
+    && docker-php-ext-install -j$(nproc) gd
 
 # Install Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
